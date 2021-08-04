0565a1738f7e68bb62ffb5467f6cc0b0f7f53cef
Arjun Yuvaraj <arjunycoding@gmail.com>
2021-08-04 11:11:34 -0400

    finsihed app.py

--- app.py
+++ app.py
@@ -1,6 +1,31 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:a.yuvAc*@localhost/sticky'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class AccountInfo(db.Model):
+    __tablename__ = 'accountInfo'
+    email = db.Column(db.String(200), primary_key=True)
+    password = db.Column(db.String(200))
+    username = db.Column(db.String(200), primary_key=True)
+
+    def __init__(self, email, password, username):
+        self.email = email
+        self.password = password
+        self.username = username
 
 @app.route('/')
 def index():
@@ -17,10 +42,23 @@ def createaccount():
     username = request.form['username']
     if email == "" or password == "" or username == "":
         return render_template('signUp.html', message="Please enter requiered fields")
-    return render_template('login.html')
+    # if 
+    if db.session.query(AccountInfo).filter(AccountInfo.username == username).count() == 0 and db.session.query(AccountInfo).filter(AccountInfo.email == email).count() == 0:
+        data = AccountInfo(email, password, username)
+        db.session.add(data)
+        db.session.commit()
+        return render_template('login.html')
+
+    return render_template('signUp.html', message="Sorry! Your username or email was already taken")
+
 
 @app.route('/mainpage', methods=['POST'])
 def mainpage():
+    username = request.form['username']
+    password = request.form['password']
+    user = AccountInfo.query.filter_by(username=username, password=password).first()
+    if user == None:
+        return render_template('login.html', message="You have entered the wrong username or password")
     return render_template('index.html')
 
 if __name__ == '__main__':
